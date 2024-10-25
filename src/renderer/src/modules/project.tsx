ec5e829b0224e8d8197dc354302845f2cc68bfe9
Lu Yifei <118034848+LuYifei2011@users.noreply.github.com>
2024-10-26 07:13:01 +0800

    更新 project.tsx

--- src/renderer/src/modules/project.tsx
+++ src/renderer/src/modules/project.tsx
@@ -1,91 +1,106 @@
-import React, { ReactElement, useState } from "react";
+import React, { useState, useEffect, ReactElement, useRef } from "react";
 import ReactDOM from 'react-dom/client';
 import { TabList, Tab } from "@fluentui/react-components";
-import type {
-    SelectTabData,
-    SelectTabEvent,
-    TabValue,
-} from "@fluentui/react-components";
+import type { SelectTabData, SelectTabEvent, TabValue } from "@fluentui/react-components";
 
-export class Project {
-    mainDiv: HTMLDivElement;
-    private tabs: ReactElement[] = [];
-    const [selectedValue, setSelectedValue] =
-    React.useState<TabValue>("conditions");
+const ProjectComponent = React.forwardRef(({ mainDiv }: { mainDiv: HTMLDivElement }, ref) => {
+    const [tabs, setTabs] = useState<ReactElement[]>([]);
+    const [selectedValue, setSelectedValue] = useState<TabValue>("conditions");
 
     const onTabSelect = (event: SelectTabEvent, data: SelectTabData) => {
         setSelectedValue(data.value);
     };
 
-    constructor(mainDiv: HTMLDivElement) {
-        this.mainDiv = mainDiv;
-    }
+    useEffect(() => {
+        initializeTab();
+    }, []);
 
-    private initializeTab(): void {
-        this.tabs = [];
-        this.renderTabs();
-    }
+    const initializeTab = () => {
+        setTabs([]);
+        renderTabs();
+    };
 
-    private newTab(value: string, content: string): void {
-        const newTabElement = <Tab value={value} content={content} />;
-        this.tabs.push(newTabElement);
-        this.renderTabs();
-    }
+    const newTab = (value: string, content: string) => {
+        const newTabElement = <Tab key={value} value={value} content={content} />;
+        setTabs(prevTabs => [...prevTabs, newTabElement]);
+        renderTabs();
+    };
 
-    private renderTabs(): void {
+    const renderTabs = () => {
         const tabList = (
-            <TabList selectedValue={this.tabs[0]? this.tabs[0].props.value : ""}>
-                {this.tabs}
+            <TabList selectedValue={tabs[0] ? tabs[0].props.value : ""} onTabSelect={onTabSelect}>
+                {tabs}
             </TabList>
         );
-        ReactDOM.createRoot(this.mainDiv).render(tabList);
-    }
+        ReactDOM.createRoot(mainDiv).render(tabList);
+    };
 
-    getInfo(): any {
+    const getInfo = () => {
         return {
             id: 'project-id',
             name: 'Project Name',
             version: '1.0.0'
         };
-    }
+    };
 
-    codePreview(): void {
+    const codePreview = () => {
         // TODO: Implement code preview
-    }
+    };
 
-    initWorkspace(): void {
+    const initWorkspace = () => {
         // TODO: Inject workspace initialization code
         // TODO: Load Msg
         // TODO: Load Plugin
-    }
+    };
 
-    initTab(): void {
-        this.initializeTab();
-        this.newTab("Blocks", "Blocks");
-        this.initWorkspace();
-        this.codePreview();
-    }
+    const initTab = () => {
+        initializeTab();
+        newTab("Blocks", "Blocks");
+        initWorkspace();
+        codePreview();
+    };
 
-    load(data: any): void {
-        this.initTab();
-    }
+    const load = (data: any) => {
+        initTab();
+    };
 
-    save(): any {
+    const save = () => {
         // TODO: Save project
         return {};
-    }
+    };
 
-    new(): any {
+    const createNew = () => {
         // TODO: Create new project
         return {};
-    }
+    };
 
-    generateCode(): string {
+    const generateCode = () => {
         // TODO: Generate code for project
         return "";
-    }
+    };
 
-    run(): void {
+    const run = () => {
         // TODO: Run project
-    }
-}
+    };
+
+    // Expose methods to parent component
+    React.useImperativeHandle(ref, () => ({
+        load,
+        save,
+        createNew,
+        generateCode,
+        run,
+        getInfo,
+        codePreview,
+        initWorkspace,
+        initTab,
+    }));
+
+    return (
+        <div>
+            {/* Your component JSX structure */}
+        </div>
+    );
+});
+
+export default ProjectComponent;
